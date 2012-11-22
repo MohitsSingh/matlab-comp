650d8084578c40cf69ff3d1be56019c134e8dd40
cyy82073@gmail.com <cyy82073@gmail.com@59f7c9e3-4226-1427-24a2-cfab09ce47be>
2012-11-22 20:45:32 +0000

    function drawF2(xPixels,yPixels,SNR_CoMP,SNR_hard,BS_tx_power,resolution) SNR_diff=SNR_CoMP-SNR_hard;
    for xPixel_number_counter=1:xPixels
        for yPixel_number_counter= 1:yPixels
            if(SNR_diff(yPixel_number_counter,xPixel_number_counter)<-4)
                c1=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'+','k');
    
            elseif(-4<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<-2)
                c2=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'.','c');
    
            elseif(-2<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<0)
                c3=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'s','m');
    
            elseif(0<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<1)
                c4=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'p','y');
    
            elseif(1<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<2)
                c5=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'h','b');
    
            elseif(2<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<3)
                c6=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'o','g');
    
            else
                c7=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'*','r');
    
            end
             hold on;
        end
    end
        
    xlabel('x-pixels(m)');
    ylabel('x-pixels(m)');
    title(['Power =',num2str(BS_tx_power),'dbm']);
    legend([c1,c2,c3,c4,c5,c6,c7],'CoMP-HHO<-4dB', '[-4,-2]dB', '[-2, 0]dB', '[0,1]dB', '[1,2]dB', '[2,3]dB', 'more than 3dB');
    hold off;
    end

--- trunk/CoMP/power/drawF2.m
+++ trunk/CoMP/power/drawF2.m
@@ -1,24 +1,24 @@
-function drawF(xPixels,yPixels,SNR_CoMP,SNR_hard,BS_tx_power,resolution)
+function drawF2(xPixels,yPixels,SNR_CoMP,SNR_hard,BS_tx_power,resolution)
 SNR_diff=SNR_CoMP-SNR_hard;
 
 for xPixel_number_counter=1:xPixels
     for yPixel_number_counter= 1:yPixels
-        if(SNR_diff(yPixel_number_counter,xPixel_number_counter)<0)
+        if(SNR_diff(yPixel_number_counter,xPixel_number_counter)<-4)
             c1=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'+','k');
 
-        elseif(0<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<1)
+        elseif(-4<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<-2)
             c2=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'.','c');
 
-        elseif(1<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<2)
+        elseif(-2<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<0)
             c3=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'s','m');
 
-        elseif(2<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<3)
+        elseif(0<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<1)
             c4=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'p','y');
 
-        elseif(3<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<4)
+        elseif(1<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<2)
             c5=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'h','b');
 
-        elseif(4<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<5)
+        elseif(2<=SNR_diff(yPixel_number_counter,xPixel_number_counter) && SNR_diff(yPixel_number_counter,xPixel_number_counter)<3)
             c6=scatter(xPixel_number_counter*resolution,yPixel_number_counter*resolution,'o','g');
 
         else
@@ -32,7 +32,6 @@ end
 xlabel('x-pixels(m)');
 ylabel('x-pixels(m)');
 title(['Power =',num2str(BS_tx_power),'dbm']);
-legend([c1,c2,c3,c4,c5,c6,c7],'CoMP SNR less than HHO', '1 dB larger', '2 dB larger', '3 dB larger', '4 dB larger', '5 dB larger', 'more than 5dB');
+legend([c1,c2,c3,c4,c5,c6,c7],'CoMP-HHO<-4dB', '[-4,-2]dB', '[-2, 0]dB', '[0,1]dB', '[1,2]dB', '[2,3]dB', 'more than 3dB');
 hold off;
 end
-
